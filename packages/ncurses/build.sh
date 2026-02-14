0991d2e170856f86a98d603cdf648bbed8e142b9
Frank <mail@franku.nl>
2026-02-14 19:15:42 +0100

    Fix ncurses symlinks

--- packages/ncurses/build.sh
+++ packages/ncurses/build.sh
@@ -10,50 +10,50 @@ make install
 # Change working directory to package path to create symlinks
 cd $PACKIT_PACKAGE_PATH
 
-ln -s lib/libformw.6.dylib lib/libform.dylib
-ln -s lib/libformw.6.dylib lib/libform.6.dylib
-ln -s lib/libformw.a lib/libform.a
-ln -s lib/libformw_g.a lib/libform_g.a
+ln -s libformw.6.dylib lib/libform.dylib
+ln -s libformw.6.dylib lib/libform.6.dylib
+ln -s libformw.a lib/libform.a
+ln -s libformw_g.a lib/libform_g.a
 
-ln -s lib/libmenuw.6.dylib lib/libmenu.dylib
-ln -s lib/libmenuw.6.dylib lib/libmenu.6.dylib
-ln -s lib/libmenuw.a lib/libmenu.a
-ln -s lib/libmenuw_g.a lib/libmenu_g.a
+ln -s libmenuw.6.dylib lib/libmenu.dylib
+ln -s libmenuw.6.dylib lib/libmenu.6.dylib
+ln -s libmenuw.a lib/libmenu.a
+ln -s libmenuw_g.a lib/libmenu_g.a
 
-ln -s lib/libncursesw.6.dylib lib/libncurses.dylib
-ln -s lib/libncursesw.6.dylib lib/libncurses.6.dylib
-ln -s lib/libncursesw.a lib/libncurses.a
-ln -s lib/libncursesw_g.a lib/libncurses_g.a
+ln -s libncursesw.6.dylib lib/libncurses.dylib
+ln -s libncursesw.6.dylib lib/libncurses.6.dylib
+ln -s libncursesw.a lib/libncurses.a
+ln -s libncursesw_g.a lib/libncurses_g.a
 
-ln -s lib/libpanelw.6.dylib lib/libpanel.dylib
-ln -s lib/libpanelw.6.dylib lib/libpanel.6.dylib
-ln -s lib/libpanelw.a lib/libpanel.a
-ln -s lib/libpanelw_g.a lib/libpanel_g.a
+ln -s libpanelw.6.dylib lib/libpanel.dylib
+ln -s libpanelw.6.dylib lib/libpanel.6.dylib
+ln -s libpanelw.a lib/libpanel.a
+ln -s libpanelw_g.a lib/libpanel_g.a
 
-ln -s lib/libncurses++w.6.dylib lib/libncurses++.dylib
-ln -s lib/libncurses++w.6.dylib lib/libncurses++.6.dylib
-ln -s lib/libncurses++w.a lib/libncurses++.a
-ln -s lib/libncurses++w_g.a lib/libncurses++_g.a
+ln -s libncurses++w.6.dylib lib/libncurses++.dylib
+ln -s libncurses++w.6.dylib lib/libncurses++.6.dylib
+ln -s libncurses++w.a lib/libncurses++.a
+ln -s libncurses++w_g.a lib/libncurses++_g.a
 
-ln -s lib/libncurses.a lib/libcurses.a
-ln -s lib/libncurses.dylib lib/libcurses.dylib
+ln -s libncurses.a lib/libcurses.a
+ln -s libncurses.dylib lib/libcurses.dylib
 
 if [[ $PACKIT_TARGET =~ "linux" ]]; then
-    ln -s lib/libncurses.so lib/libtermcap.so
-    ln -s lib/libncurses.a lib/libtinfo.so
+    ln -s libncurses.so lib/libtermcap.so
+    ln -s libncurses.a lib/libtinfo.so
 fi
 
-ln -s lib/pkgconfig/ncursesw.pc lib/pkgconfig/ncurses.pc
-ln -s lib/pkgconfig/formw.pc lib/pkgconfig/form.pc
-ln -s lib/pkgconfig/menuw.pc lib/pkgconfig/menu.pc
-ln -s lib/pkgconfig/panelw.pc lib/pkgconfig/panel.pc
+ln -s pkgconfig/ncursesw.pc lib/pkgconfig/ncurses.pc
+ln -s pkgconfig/formw.pc lib/pkgconfig/form.pc
+ln -s pkgconfig/menuw.pc lib/pkgconfig/menu.pc
+ln -s pkgconfig/panelw.pc lib/pkgconfig/panel.pc
 
-ln -s bin/ncursesw6-config bin/ncurses6-config
+ln -s ncursesw6-config bin/ncurses6-config
 
-ln -s include/ncursesw include/ncurses
-ln -s include/ncursesw/curses.h include/curses.h
-ln -s include/ncursesw/form.h include/form.h
-ln -s include/ncursesw/ncurses.h include/ncurses.h
-ln -s include/ncursesw/panel.h include/panel.h
-ln -s include/ncursesw/term.h include/term.h
-ln -s include/ncursesw/termcap.h include/termcap.h
+ln -s ncursesw include/ncurses
+ln -s ncursesw/curses.h include/curses.h
+ln -s ncursesw/form.h include/form.h
+ln -s ncursesw/ncurses.h include/ncurses.h
+ln -s ncursesw/panel.h include/panel.h
+ln -s ncursesw/term.h include/term.h
+ln -s ncursesw/termcap.h include/termcap.h
